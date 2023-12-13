5c4ae640398186f776f2630fba335ca1aa99356d
Bobozia <sokoliukbohdan@gmail.com>
2023-12-13 21:37:32 +0100

    Fixed loading state on `Events` page

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllEvents } from "../api/event";
 import EventsGrid from "../components/EventsGrid";
+
 function Events() {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -8,18 +9,19 @@ function Events() {
   useEffect(() => {
     if (events.length === 0) {
       getEvents();
-      setLoading(false);
     }
   });
 
   const getEvents = async () => {
     const response = await getAllEvents();
     setEvents(response.data.events);
+    setLoading(false);
   };
+
   return (
     <div className="h-full w-full pt-16 overflow-y-scroll">
       {loading && <p>Loading...</p>}
-      {!loading && !events.length && <p>There are no events :P</p>}
+      {!loading && events.length == 0 && <p>There are no events :P</p>}
       <EventsGrid events={events} />
     </div>
   );
